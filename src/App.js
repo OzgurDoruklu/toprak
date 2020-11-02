b564da948fd4521111982895dd71a9478a18f629
OzgurDoruklu <ozgurdoruklu@gmail.com>
2020-11-02 13:20:52 +0300

    img Alt

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={MainPhoto}/>
+        <img src={MainPhoto} alt="Toprak"/>
       </header>
     </div>
   );
